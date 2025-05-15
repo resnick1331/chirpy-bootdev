dd96d33586b1b99d5a4efc05950e4697ea2090b6
resnick1331 <andresleal7@gmail.com>
2025-05-15 23:27:28 +0000

    cusmtom hanlder as per lesson CH1 L11

--- main.go
+++ main.go
@@ -6,24 +6,44 @@ import (
 )
 
 func main() {
+
+	const port  = "8080"
 	// Create a new http.ServeMux
 	mux := http.NewServeMux()
 
+	// add readines endpoint
+	mux.HandleFunc("/healthz", healthzHandler)
+
+	// update fileserver path
+	//assetHandler := http.FileServer(http.Dir("./assets"))
+	//mux.Handle("/app/", http.StripPrefix("/app", assetHandler))
+
+	appHandler := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", appHandler))
+
+
 	//crete a file server
-	fs := http.FileServer(http.Dir("."))
+	//fs := http.FileServer(http.Dir("."))
 
 	//handle request to root path
-	mux.Handle("/", fs)
+	//mux.Handle("/", fs)
 
 	//create a new http.Server struct
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: ":" + port,
 		Handler: mux,
 	}
 
-	log.Println("Server listening on http://localhost:8080")
+	log.Printf("Serving files from %s on port: %s\n", appHandler,port)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatalf("could not start server: %v", err)
 	}
+}
+
+// healthzHandler is the handler for the /healthz endpoint
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
